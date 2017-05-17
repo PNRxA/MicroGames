e7189ab0399a192c72a96e26f9090a743b63c352
pnrxa <pnrunham@gmail.com>
2017-05-17 13:44:58 +1000

    Initialised Repository

---
+++ Assets/MicroGames/Peter_Runham/Game 01/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.MicroGames.Peter_Runham
+{
+    public class PlayerMove : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Movement();
+        }
+
+        void Movement()
+        {
+            int stirlingsPatience = 50;
+            Vector3 move = new Vector3(Input.GetAxis("Horizontal") / stirlingsPatience, Input.GetAxis("Vertical") / stirlingsPatience, 0);
+            transform.Translate(move);
+        }
+    }
+}
